9026b8e3581eed730bc318bbdec828f8833a290c
YKSoon <ksyong1990@gmail.com>
2020-05-07 18:00:57 +0900

    2020_05_07_Flask

--- routes.py
+++ routes.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
-@app.route('/')
-def student():
-    return render_template('student.html')
+@app.route('/upload')
+def load_file():
+    return render_template('upload.html')
 
-@app.route('/result', methods = ['POST', 'GET'])
-def result():
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
     if request.method == 'POST':
-        result = request.form
-        return render_template("result.html", result = result)
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
